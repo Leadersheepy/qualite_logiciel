a8f635bac9f5d12530e93625917ab7cbacbd6137
Marinounouh <m.kazemi@H3hitema.fr>
2024-06-27 14:58:33 +0200

    N'EST CE PAS ?

--- test_script.py
+++ test_script.py
@@ -1,5 +1,5 @@
 def add(a, b):
-    return a - b  # Erreur ici, cela devrait être 'a + b'
+    return a - b  # Erreur ici, cela devrait être 'a + b', n'est ce pas?
 
 def subtract(a, b):
     return a + b  # Erreur ici, cela devrait être 'a - b'
